12719463a4381a0b1d0c4e996c94a44e2f0c25c3
ziadkadry99 <55473879+ziadkadry99@users.noreply.github.com>
2020-01-28 18:21:46 -0600

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,6 @@
-// KadryAssign5_335.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
 
 #include <iostream>
-//#include "CLex.h"
-//#include "CToken.h"
 #include <string>
 #include <fstream>
 #include "CLex.h"
@@ -15,29 +12,10 @@ using namespace std;
 int main()
 
 {
-
 	CLex LEX("code.txt");
 	TDCParser Parser;
-
 	Parser.Parse(&LEX);
-
-	/*CLex LEX("code.txt");
-	
-
-	CToken TOKEN;
-	
-
-	
-	while (LEX.GetToken(TOKEN))
-
-	{
-		cout << TOKEN.tokenValueName() << " " << TOKEN.tokenValue << endl;
-
-	}*/
-
-
 	system("pause");
-
 	return 0;
 
 }
